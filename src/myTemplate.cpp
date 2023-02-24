9c8349c0831ffbff4aa3ca3130bb419bfafdf4fc
starthemoon <812138420@qq.com>
2023-02-24 14:30:22 +0800

    update .gitignore

--- src/myTemplate.cpp
+++
@@ -1,9 +0,0 @@
-#include "myTemplate.h"
-
-template <typename T>
-void MyClass<T>::getData() {
-    cout << typeid(T).name() << endl;
-}
-
-template class MyClass<int>;
-template class MyClass<double>;
